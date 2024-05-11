ac29c2de960f28a1a8eed0424e0a9d1fac94b2bc
Zaghdoudi mohamed <mohamed.zaghdoudi@codeonce.fr>
2024-05-11 11:10:49 +0100

    create SpringBoot project

---
+++ crud-app/src/test/java/com/med/crudapp/CrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.crudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
